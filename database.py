79e6b6b328fd7ae58a4169b46ddf9b9c2a06571a
Ben Sidi EL-HOUDHAIFFOUDDINE <bensidi.elhoudhaiffouddine@esprit.tn>
2025-04-16 17:26:16 +0000

    Updating files: compose.yaml database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from Gender import Gender
 from sqlalchemy import create_engine, text
 
 username = "ben"
-password = "azerty"
+password = "weak_password"
 
 # Modify the below IP to add the IP of your database and add a port number if necessary
 engine = create_engine(f"mysql+pymysql://{username}:{password}@192.168.1.3")
